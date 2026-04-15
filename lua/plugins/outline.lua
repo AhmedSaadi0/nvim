38a49e1da441d7306538e765008e7e0667c9a984
ahmedSaadi0 <cr9ine@gmail.com>
2026-04-15 16:34:52 +0300

    add opencode config & remove opencode nvim plugin to use native app

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -3,7 +3,7 @@ return {
 	lazy = true,
 	cmd = { "Outline", "OutlineOpen" },
 	keys = {
-		{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+		{ "<leader>c", "<cmd>Outline<CR>", desc = "Toggle outline" },
 	},
 	opts = {
 		providers = {
